aed12888ccce6e75906aa1e890943166dcbef5e9
Eduardo González <eduardo_gonzm@hotmail.com>
2024-11-07 16:39:57 -0600

    This is the code for my 2D Treasure Hunt Game

---
+++ My2DGame/src/main/GamePanel.java
@@ -0,0 +1,139 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JPanel;
+
+import entity.Player;
+import object.SuperObject;
+import tile.TileManager;
+
+public class GamePanel extends JPanel implements Runnable {
+
+	// SCREEN SETTINGS
+	final int originalTileSize = 16; // 16x16 tile
+	final int scale = 3;
+	
+	public final int tileSize = originalTileSize * scale; // 48x48 tile
+	public final int maxScreenCol = 16;
+	public final int maxScreenRow = 12;
+	public final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+	public final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+	
+	// WORLD SETTINGS
+	public final int maxWorldCol = 50;
+	public final int maxWorldRow = 50;
+	
+	// FPS
+	int FPS = 60;
+	
+	//SYSTEM
+	TileManager tileM = new TileManager(this);
+	KeyHandler keyH = new KeyHandler();
+	Sound music = new Sound();
+	Sound se = new Sound();
+	public CollisionChecker cChecker = new CollisionChecker(this);
+	public AssetSetter aSetter = new AssetSetter(this);
+	public UI ui = new UI(this);
+	Thread gameThread;
+	
+	//ENTITY AND OBKECT
+	public Player player = new Player(this, keyH);
+	public SuperObject obj[] = new SuperObject[10];
+	
+	
+	public GamePanel() {
+		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+		this.setBackground(Color.black);
+		this.setDoubleBuffered(true);
+		this.addKeyListener(keyH);
+		this.setFocusable(true);
+	}
+	
+	public void setupGame() {
+		aSetter.setObject();
+		
+		playMusic(0);
+	}
+	
+	public void startGameThread() {
+		gameThread = new Thread(this);
+		gameThread.start();
+	}
+	
+	public void run() {
+		
+		double drawInterval = 1000000000/FPS; // Nanoseconds are used
+		double nextDrawTime = System.nanoTime() + drawInterval;
+		
+		while (gameThread != null) {
+			
+			// 1 UPDATE: update information such as character positions
+			update();
+			
+			// 2 DRAW: draw the screen with the updated information
+			repaint();
+			
+			try {
+				double remainingTime = nextDrawTime - System.nanoTime();
+				remainingTime = remainingTime / 1000000;
+				
+				if (remainingTime < 0) {
+					remainingTime = 0;
+				}
+				
+				Thread.sleep((long) remainingTime);
+				
+				nextDrawTime += drawInterval;
+				
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+		}
+	}
+	
+	public void update() {
+		player.update();
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D)g;
+		// TILE
+		tileM.draw(g2);
+		
+		//OBJECT
+		for(int i = 0; i < obj.length; i++) {
+			if (obj[i] != null) {
+				obj[i].draw(g2, this);
+			}
+		}
+		
+		// PLAYER
+		player.draw(g2);
+		
+		// UI
+		ui.draw(g2);
+		
+		g2.dispose();
+	}
+	
+	public void playMusic(int i) {
+		music.SetFile(i);
+		music.play();
+		music.loop();
+	}
+	
+	public void stopMusic() {
+		music.stop();
+	}
+	
+	public void playSE(int i) {
+		se.SetFile(i);
+		se.play();
+	}
+}
